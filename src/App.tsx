b6145cda93243d303d3d7261948e8ae09daee45d
gshohat <gilad.shohat@gmail.com>
2022-01-07 11:42:26 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import alfajorSmile from './alfajor-smile.png';
 import './App.css';
 
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const ALFAJORES_PARAMS = {
+  chainId: '0xaef3',
+  chainName: 'Alfajores Testnet',
+  nativeCurrency: { name: "Alfajores Celo", symbol: 'A-CELO', decimals: 18 },
+  rpcUrls: ['https://alfajores-forno.celo-testnet.org'],
+  blockExplorerUrls: ['https://alfajores-blockscout.celo-testnet.org/'],
+  iconUrls: ['future'],
+};
+
+async function startApp(provider: any) {
+  if (provider !== window.ethereum) {
+   return console.error('Do you have multiple wallets installed?');
+  }
+  console.log('window.ethereum', window.ethereum);
+  const res = await provider.request({
+    method: 'wallet_addEthereumChain',
+    params: [ALFAJORES_PARAMS],
+  });
+  console.log('res', res);
+
+  const resWatch = await provider.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20',
+      options: {
+        address: '0x874069fa1eb16d44d622f2e0ca25eea172369bc1',
+        'symbol': 'cUSD',
+        decimals: 18,
+      },
+      iconUrls: ['future'],
+    },
+  });
+  console.log('resWatch', resWatch);
+}
+
 function App() {
+
+  function addTestnet() {
+    detect();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={alfajorSmile} className="App-logo" alt="logo" />
       </header>
+      <button onClick={() => addTestnet()}>Add Testnet</button>
     </div>
   );
 }
+
+const detect = async() => {
+  const provider = await detectEthereumProvider();
+  if (provider) {
+    await startApp(provider);
+  } else {
+    console.log('Please install MetaMask!');
+  }
+};
 
 export default App;
